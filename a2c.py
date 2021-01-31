Fix a bug that causes a silent failure
- Fix bug in check_episodes() that prevents updating metrics and causes a silent failure.
- Add init_training() that will be called before training all agents
- Optimizers can be specified through optimizer=tf_compatible_optimizer
- Update ppo.py with experimental code

a2c.py
@@ -46,24 +46,31 @@ class A2C(BaseAgent):
         """
         return np.array(self.states, np.float32)
 
-    def step_transitions(self, log_probs, values, rewards, masks, entropies):
-        states = tf.numpy_function(func=self.get_states, inp=[], Tout=tf.float32)
+    def step_transitions(
+        self, states, actions, log_probs, values, rewards, masks, entropies
+    ):
+        step_states = tf.numpy_function(func=self.get_states, inp=[], Tout=tf.float32)
         for step in range(self.transition_steps):
-            actions, step_log_probs, step_entropies, step_values = self.model(states)
-            states, step_rewards, step_dones = tf.numpy_function(
+            step_actions, step_log_probs, step_entropies, step_values = self.model(
+                step_states
+            )
+            step_states, step_rewards, step_dones = tf.numpy_function(
                 func=self.step_envs,
-                inp=[actions],
+                inp=[step_actions],
                 Tout=(tf.float32, tf.float32, tf.float32),
             )
             step_masks = 1 - step_dones
+            states.append(step_states)
+            actions.append(step_actions)
             log_probs.append(step_log_probs)
             values.append(step_values)
             rewards.append(step_rewards)
             masks.append(step_masks)
             entropies.append(step_entropies)
-        return states
 
-    def calculate_returns(self, masks, rewards, values, log_probs, entropies):
+    def calculate_returns(
+        self, states, actions, masks, rewards, values, log_probs, entropies
+    ):
         """
         Calculate returns to be used for loss calculation and gradient update.
         Args:
@@ -81,8 +88,10 @@ class A2C(BaseAgent):
         Returns:
             returns (a list that has most recent values after performing n steps)
         """
-        states = self.step_transitions(log_probs, values, rewards, masks, entropies)
-        next_values = self.model(states)[-1]
+        self.step_transitions(
+            states, actions, log_probs, values, rewards, masks, entropies
+        )
+        next_values = self.model(states[-1])[-1]
         returns = [next_values]
         for step in reversed(range(self.transition_steps)):
             returns.insert(0, rewards[step] + masks[step] * self.gamma * returns[0])
@@ -130,6 +139,8 @@ class A2C(BaseAgent):
         Returns:
             None
         """
+        states = []
+        actions = []
         masks = []
         rewards = []
         values = []
@@ -137,11 +148,11 @@ class A2C(BaseAgent):
         entropies = []
         with tf.GradientTape() as tape:
             returns = self.calculate_returns(
-                masks, rewards, values, log_probs, entropies
+                states, actions, masks, rewards, values, log_probs, entropies
             )
             loss = self.calculate_loss(returns, values, log_probs, entropies)
         gradients = tape.gradient(loss, self.model.trainable_variables)
-        gradients, _ = tf.clip_by_global_norm(gradients, clip_norm)
+        gradients, _ = tf.clip_by_global_norm(gradients, self.clip_norm)
         self.model.optimizer.apply_gradients(
             zip(gradients, self.model.trainable_variables)
         )
@@ -151,11 +162,7 @@ class A2C(BaseAgent):
         target_reward,
         max_steps=None,
         monitor_session=None,
-        learning_rate=7e-4,
         weights=None,
-        epsilon=1e-5,
-        beta_1=0.0,
-        beta_2=0.99,
     ):
         """
         Train agent on a supported environment
@@ -163,20 +170,11 @@ class A2C(BaseAgent):
             target_reward: Target reward, if achieved, the training will stop
             max_steps: Maximum number of steps, if reached the training will stop.
             monitor_session: Session name to use for monitoring the training with wandb.
-            learning_rate: Model learning rate shared by both main and target networks.
             weights: Path to .tf trained model weights to continue training.
-            epsilon: epsilon parameter passed to tfa.optimizers.RectifiedAdam()
-            beta_1: beta_1 parameter passed tfa.optimizers.RectifiedAdam()
-            beta_2: beta_2 parameter passed to tfa.optimizers.RectifiedAdam()
         Returns:
             None
         """
-        optimizer = tfa.optimizers.RectifiedAdam(
-            learning_rate=learning_rate, epsilon=epsilon, beta_1=beta_1, beta_2=beta_2
-        )
-        self.init_training(
-            optimizer, target_reward, max_steps, monitor_session, weights, None
-        )
+        self.init_training(target_reward, max_steps, monitor_session, weights, None)
         while True:
             self.check_episodes()
             if self.training_done():
@@ -188,8 +186,11 @@ if __name__ == '__main__':
     ens = create_gym_env('PongNoFrameskip-v4', 16)
     from models import CNNA2C
 
+    o = tfa.optimizers.RectifiedAdam(
+        learning_rate=7e-4, epsilon=1e-5, beta_1=0.0, beta_2=0.99
+    )
     m = CNNA2C(ens[0].observation_space.shape, ens[0].action_space.n)
-    ac = A2C(ens, m)
+    ac = A2C(ens, m, optimizer=o)
     ac.fit(19)
     # ac.play(
     #     '/Users/emadboctor/Desktop/code/drl-models/a2c-pong-17-model/a2c-pong.tf',

base_agent.py
@@ -7,6 +7,7 @@ import cv2
 import gym
 import numpy as np
 import wandb
+from tensorflow.keras.optimizers import Adam
 
 
 class BaseAgent:
@@ -14,6 +15,7 @@ class BaseAgent:
         self,
         envs,
         model,
+        optimizer=None,
         checkpoint=None,
         reward_buffer_size=100,
         transition_steps=1,
@@ -37,6 +39,7 @@ class BaseAgent:
         self.n_envs = len(envs)
         self.envs = envs
         self.model = model
+        self.optimizer = optimizer or Adam()
         self.checkpoint_path = checkpoint
         self.total_rewards = deque(maxlen=reward_buffer_size)
         self.transition_steps = transition_steps
@@ -129,7 +132,7 @@ class BaseAgent:
         Returns:
             None
         """
-        if len(self.done_envs) == self.n_envs:
+        if len(self.done_envs) >= self.n_envs:
             self.update_metrics()
             self.last_reset_time = perf_counter()
             self.display_metrics()
@@ -184,13 +187,10 @@ class BaseAgent:
             ]
         return [np.array(item, np.float32) for item in zip(*observations)]
 
-    def init_training(
-        self, optimizer, target_reward, max_steps, monitor_session, weights, loss
-    ):
+    def init_training(self, target_reward, max_steps, monitor_session, weights, loss):
         """
         Initialize training start time, wandb session & models (self.model / self.target_model)
         Args:
-            optimizer: Optimizer passed to tf.keras.models.Model.compile(optimizer=optimizer)
             target_reward: Scalar values whenever achieved, the training will stop.
             max_steps: Maximum steps, if exceeded, the training will stop.
             monitor_session: Wandb session name.
@@ -208,9 +208,9 @@ class BaseAgent:
             self.model.load_weights(weights)
             if hasattr(self, 'target_model'):
                 self.target_model.load_weights(weights)
-        self.model.compile(optimizer, loss=loss)
+        self.model.compile(self.optimizer, loss=loss)
         if hasattr(self, 'target_model'):
-            self.target_model.compile(optimizer, loss=loss)
+            self.target_model.compile(self.optimizer, loss=loss)
         self.training_start_time = perf_counter()
         self.last_reset_time = perf_counter()
 

ppo.py
@@ -1,3 +1,5 @@
+import tensorflow as tf
+
 from a2c import A2C
 from utils import create_gym_env
 
@@ -10,6 +12,7 @@ class PPO(A2C):
         transition_steps=128,
         gae_lambda=0.95,
         ppo_epochs=4,
+        mini_batches=4,
         *args,
         **kwargs,
     ):
@@ -26,8 +29,11 @@ class PPO(A2C):
         )
         self.gae_lambda = gae_lambda
         self.ppo_epochs = ppo_epochs
+        self.mini_batches = mini_batches
 
-    def calculate_returns(self, masks, rewards, values, log_probs, entropies):
+    def calculate_returns(
+        self, states, actions, masks, rewards, values, log_probs, entropies
+    ):
         """
         Calculate returns to be used for loss calculation and gradient update.
         Args:
@@ -45,8 +51,10 @@ class PPO(A2C):
         Returns:
             returns (a list that has most recent values after performing n steps)
         """
-        states = self.step_transitions(log_probs, values, rewards, masks, entropies)
-        next_values = self.model(states)[-1]
+        self.step_transitions(
+            states, actions, log_probs, values, rewards, masks, entropies
+        )
+        next_values = self.model(states[-1])[-1]
         values.append(next_values)
         gae = 0
         returns = []
@@ -60,11 +68,91 @@ class PPO(A2C):
             returns.insert(0, gae + values[step])
         return returns
 
+    @tf.function
+    def train_step(self):
+        """
+        Do 1 training step.
+
+        Returns:
+            None
+        """
+        states = []
+        actions = []
+        masks = []
+        rewards = []
+        values = []
+        log_probs = []
+        entropies = []
+        returns = self.calculate_returns(
+            states, actions, masks, rewards, values, log_probs, entropies
+        )
+        tf_returns = tf.concat(returns, axis=0)
+        tf_observations = tf.concat(states[:-1], axis=0)
+        tf_actions = tf.concat(actions, axis=0)
+        tf_old_log_probs = tf.concat(log_probs, axis=0)
+        tf_values = tf.concat(values[:-1], axis=0)
+        tf_adv_target = tf_returns - tf_values
+        tf_adv_target = (tf_adv_target - tf.reduce_mean(tf_adv_target)) / (
+            tf.math.reduce_std(tf_adv_target) + 1e-5
+        )
+        batch_size = self.n_envs * self.transition_steps
+        mini_batch_size = batch_size // self.mini_batches
+        value_loss_epoch = 0.0
+        action_loss_epoch = 0.0
+        entropy_loss_epoch = 0.0
+        for _ in range(self.ppo_epochs):
+            indx = tf.random.shuffle(tf.range(batch_size))
+            indx = tf.reshape(indx, (-1, mini_batch_size))
+            for sample in indx:
+                obs_batch = tf.gather(tf_observations, sample)
+                returns_batch = tf.gather(tf_returns, sample)
+                adv_target_batch = tf.gather(tf_adv_target, sample)
+                action_batch = tf.gather(tf_actions, sample)
+                old_log_probs_batch = tf.gather(tf_old_log_probs, sample)
+                values_batch = tf.gather(tf_values, sample)
+                with tf.GradientTape() as tape:
+                    _, action_log_probs, dist_entropy, value = self.model(
+                        obs_batch, action_batch
+                    )
+                    ratio = tf.exp(action_log_probs - old_log_probs_batch)
+                    surr1 = -ratio * adv_target_batch
+                    surr2 = (
+                        -tf.clip_by_value(
+                            ratio, 1.0 - self.clip_norm, 1.0 + self.clip_norm
+                        )
+                        * adv_target_batch
+                    )
+                    action_loss = tf.reduce_mean(tf.maximum(surr1, surr2))
+                    value_pred_clipped = values_batch + tf.clip_by_value(
+                        value - values_batch, -self.clip_norm, self.clip_norm
+                    )
+                    value_losses = tf.square(value - returns_batch)
+                    value_losses_clipped = tf.square(value_pred_clipped - returns_batch)
+                    value_loss = 0.5 * tf.reduce_mean(
+                        tf.maximum(value_losses, value_losses_clipped)
+                    )
+                    entropy_loss = tf.reduce_mean(dist_entropy)
+                    loss = (
+                        self.value_loss_coef * value_loss
+                        + action_loss
+                        - entropy_loss * self.entropy_coef
+                    )
+                gradients = tape.gradient(loss, self.model.trainable_variables)
+                gradients, _ = tf.clip_by_global_norm(gradients, 0.5)
+                self.model.optimizer.apply_gradients(
+                    zip(gradients, self.model.trainable_variables)
+                )
+                value_loss_epoch += value_loss
+                action_loss_epoch += action_loss
+                entropy_loss_epoch += entropy_loss
+
 
 if __name__ == '__main__':
     ens = create_gym_env('PongNoFrameskip-v4', 16)
+    from tensorflow.keras.optimizers import Adam
+
     from models import CNNA2C
 
     m = CNNA2C(ens[0].observation_space.shape, ens[0].action_space.n)
-    ac = PPO(ens, m)
+    ac = PPO(ens, m, optimizer=Adam(25e-5, epsilon=1e-5))
     ac.fit(19)