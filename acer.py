4428a2a19212cc70599529803aa385c7f4d23e76
emadboctorx <emad_1989@hotmail.com>
2021-03-16 04:30:19 +0200

    Wrap everything in ACER within 1 class

--- acer.py
+++ acer.py
@@ -1,17 +1,25 @@
-import time
-
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.optimizers import Adam
 
 from a2c import A2C
 from models import CNNA2C
 from utils import ReplayBuffer, create_gym_env
 
 
-class Runner(A2C):
-    def __init__(self, env, model, n_steps=20, seed=None):
-        super(Runner, self).__init__(env, model, n_steps=n_steps, seed=seed)
+class ACER(A2C):
+    def __init__(
+        self, env, models, n_steps=20, seed=None, buffer_size=50000, ema_alpha=0.99
+    ):
+        super(ACER, self).__init__(env, models[0], n_steps=n_steps, seed=seed)
+        self.avg_model = models[1]
+        self.buffers = [
+            ReplayBuffer(buffer_size // len(env), 500, batch_size=1, seed=seed)
+            for _ in range(len(env))
+        ]
+        self.ema = tf.train.ExponentialMovingAverage(ema_alpha)
+        self.batch_indices = tf.range(self.n_steps * self.n_envs, dtype=tf.int64)[
+            :, tf.newaxis
+        ]
 
     @staticmethod
     def gradient_add(g1, g2):
@@ -59,33 +67,6 @@ class Runner(A2C):
         dones = np.asarray(dones[1:], dtype=np.float32).swapaxes(1, 0)
         return states, actions, rewards, actor_logits, dones
 
-
-class Acer:
-    def __init__(
-        self,
-        runner,
-        v2_models,
-        buffers,
-        log_interval,
-        ema_alpha=0.99,
-    ):
-        self.runner = runner
-        self.model, self.avg_model = v2_models
-        self.buffers = buffers
-        self.log_interval = log_interval
-        self.tstart = None
-        self.steps = None
-        self.ema = tf.train.ExponentialMovingAverage(ema_alpha)
-        self.training_return_names = [
-            'loss',
-            'loss_q',
-            'entropy',
-            'loss_f',
-        ]
-        self.batch_indices = tf.range(runner.n_steps * runner.n_envs, dtype=tf.int64)[
-            :, tf.newaxis
-        ]
-
     def update_avg_weights(self):
         avg_variables = [
             self.ema.average(weight).numpy()
@@ -95,7 +76,7 @@ class Acer:
 
     def concat_buffer_samples(self):
         batches = []
-        for i in range(self.runner.n_envs):
+        for i in range(self.n_envs):
             batch = self.buffers[i].get_sample()
             batches.append(batch)
         if len(batches) > 1:
@@ -104,9 +85,9 @@ class Acer:
 
     def call(self, on_policy):
         if on_policy:
-            batch = self.runner.run()
+            batch = self.run()
             if self.buffers is not None:
-                for i in range(self.runner.n_envs):
+                for i in range(self.n_envs):
                     env_outputs = []
                     for item in batch:
                         env_outputs.append(item[i])
@@ -118,12 +99,6 @@ class Acer:
             obs, actions, rewards, mus, dones = self.concat_buffer_samples()
         self.train(obs, actions, mus, rewards, dones)
         self.update_avg_weights()
-        if on_policy and (
-            int(self.steps / (self.runner.n_steps * self.runner.n_envs))
-            % self.log_interval
-            == 0
-        ):
-            self.runner.display_metrics()
 
     @tf.function
     def train(
@@ -148,16 +123,16 @@ class Acer:
             *_, train_q, _, train_model_p = self.model(obs)
             *_, polyak_q, _, polyak_model_p = self.avg_model(obs)
             v = tf.reduce_sum(input_tensor=train_model_p * train_q, axis=-1)
-            f = tf.squeeze(train_model_p[: -self.runner.n_envs])
-            f_pol = tf.squeeze(polyak_model_p[: -self.runner.n_envs])
-            q = tf.squeeze(train_q[: -self.runner.n_envs])
+            f = tf.squeeze(train_model_p[: -self.n_envs])
+            f_pol = tf.squeeze(polyak_model_p[: -self.n_envs])
+            q = tf.squeeze(train_q[: -self.n_envs])
             f_i = tf.gather_nd(f, action_indices)
             q_i = tf.gather_nd(q, action_indices)
             rho = f / (mus + eps)
             rho_i = tf.gather_nd(rho, action_indices)
-            qret = self.runner.q_retrace(rewards, dones, q_i, v, rho_i)
+            qret = self.q_retrace(rewards, dones, q_i, v, rho_i)
             entropy = tf.reduce_mean(-tf.reduce_sum(f * tf.math.log(f + eps), axis=1))
-            v = v[: -self.runner.n_envs]
+            v = v[: -self.n_envs]
             adv = qret - v
             logf = tf.math.log(f_i + eps)
             gain_f = logf * tf.stop_gradient(adv * tf.minimum(c, rho_i))
@@ -169,11 +144,7 @@ class Acer:
                 * q_coef
             )
             if trust_region:
-                loss = (
-                    -(loss_f - ent_coef * entropy)
-                    * self.runner.n_steps
-                    * self.runner.n_envs
-                )
+                loss = -(loss_f - ent_coef * entropy) * self.n_steps * self.n_envs
             else:
                 loss = loss_f + q_coef * loss_q - ent_coef * entropy
         if trust_region:
@@ -187,13 +158,12 @@ class Acer:
                 (tf.reduce_sum(input_tensor=k * g, axis=-1) - delta)
                 / (tf.reduce_sum(input_tensor=tf.square(k), axis=-1) + eps),
             )
-            g = g - tf.reshape(adj, [self.runner.n_envs * self.runner.n_steps, 1]) * k
-            grads_f = -g / (self.runner.n_envs * self.runner.n_steps)
+            g = g - tf.reshape(adj, [self.n_envs * self.n_steps, 1]) * k
+            grads_f = -g / (self.n_envs * self.n_steps)
             grads_policy = tape.gradient(f, self.model.trainable_variables, grads_f)
             grads_q = tape.gradient(loss_q, self.model.trainable_variables)
             grads = [
-                self.runner.gradient_add(g1, g2)
-                for (g1, g2) in zip(grads_policy, grads_q)
+                self.gradient_add(g1, g2) for (g1, g2) in zip(grads_policy, grads_q)
             ]
         else:
             grads = tape.gradient(loss, self.model.trainable_variables)
@@ -206,13 +176,9 @@ class Acer:
 def learn(
     env,
     seed=None,
-    n_steps=20,
     total_timesteps=int(80e6),
-    log_interval=100,
-    buffer_size=50000,
     replay_ratio=4,
 ):
-    n_envs = len(env)
     ms = [
         CNNA2C(
             (84, 84, 1),
@@ -224,23 +190,15 @@ def learn(
         )
         for _ in range(2)
     ]
-    ms[0].compile(optimizer=Adam())
-    runner = Runner(env=env, model=ms[0], seed=seed)
-    runner.init_training(19, *[None] * 3)
-    buffers = [
-        ReplayBuffer(buffer_size // len(env), 500, batch_size=1, seed=seed)
-        for _ in range(len(env))
-    ]
-    n_batches = n_envs * n_steps
-    acer = Acer(runner, ms, buffers, log_interval)
-    acer.tstart = time.time()
-    for acer.steps in range(0, total_timesteps, n_batches):
-        acer.call(True)
-        runner.check_episodes()
-        if replay_ratio > 0 and len(buffers[0]) >= buffers[0].initial_size:
+    agn = ACER(env, ms, seed=seed)
+    agn.init_training(19, *[None] * 3)
+    for _ in range(0, total_timesteps):
+        agn.call(True)
+        agn.check_episodes()
+        if replay_ratio > 0 and len(agn.buffers[0]) >= agn.buffers[0].initial_size:
             n = np.random.poisson(replay_ratio)
             for _ in range(n):
-                acer.call(False)
+                agn.call(False)
 
 
 if __name__ == '__main__':
