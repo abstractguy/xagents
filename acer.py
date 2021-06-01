abstractguy/xagents

commit 21ebb9ec224d0afe71cd1a0e96b3966f11848638

Update base_agents.py to support separate actor and critic weight loading / saving and remove td3 scratch.

base_agents.py
@@ -12,7 +12,9 @@ import tensorflow as tf
 import wandb
 from gym.spaces.box import Box
 from gym.spaces.discrete import Discrete
+
 from utils import ReplayBuffer
+from pathlib import Path
 
 
 class BaseAgent(ABC):
@@ -52,7 +54,7 @@ class BaseAgent(ABC):
         self.n_envs = len(envs)
         self.envs = envs
         self.model = model
-        self.checkpoint_path = checkpoint
+        self.checkpoint_path = Path(checkpoint) if checkpoint else checkpoint
         self.total_rewards = deque(maxlen=reward_buffer_size)
         self.n_steps = n_steps
         self.gamma = gamma
@@ -82,6 +84,7 @@ class BaseAgent(ABC):
             np.random.seed(seed)
             for env in self.envs:
                 env.seed(seed)
+                env.action_space.seed(seed)
             os.environ['PYTHONHASHSEED'] = f'{seed}'
             random.seed(seed)
         self.reset_envs()
@@ -114,7 +117,19 @@ class BaseAgent(ABC):
         if self.mean_reward > self.best_reward:
             print(f'Best reward updated: {self.best_reward} -> {self.mean_reward}')
             if self.checkpoint_path:
-                self.model.save_weights(self.checkpoint_path)
+                if isinstance(self.output_models, (list, tuple)):
+                    actor_weights_path = (
+                        self.checkpoint_path.parent
+                        / f'actor-{self.checkpoint_path.name}'
+                    )
+                    critic_weights_path = (
+                        self.checkpoint_path.parent
+                        / f'critic-{self.checkpoint_path.name}'
+                    )
+                    self.output_models[0].save_weights(actor_weights_path.as_posix())
+                    self.output_models[1].save_weights(critic_weights_path.as_posix())
+                else:
+                    self.output_models.save_weights(self.checkpoint_path.as_posix())
         self.best_reward = max(self.mean_reward, self.best_reward)
 
     def display_metrics(self):
@@ -262,13 +277,13 @@ class BaseAgent(ABC):
 
     def init_training(self, target_reward, max_steps, monitor_session, weights):
         """
-        Initialize training start time, wandb session & models (self.model / self.target_model)
+        Initialize training start time, wandb session & models (main and target models if any)
         Args:
             target_reward: Total reward per game value that whenever achieved,
                 the training will stop.
             max_steps: Maximum time steps, if exceeded, the training will stop.
             monitor_session: Wandb session name.
-            weights: Path to .tf weights file compatible with self.model
+            weights: Path(s) to .tf weights file(s) compatible with self.output_models.
 
         Returns:
             None
@@ -277,7 +292,20 @@ class BaseAgent(ABC):
         self.max_steps = max_steps
         if monitor_session:
             wandb.init(name=monitor_session)
-        if weights:
+        if isinstance(weights, (list, tuple)):
+            assert isinstance(
+                self.output_models, (list, tuple)
+            ), 'Multiple weights provided for a single model in output_models'
+            assert len(weights) == len(
+                self.output_models
+            ), f'Expected {len(self.output_models)} weights got {len(weights)}'
+            self.output_models[0].load_weights(weights[0])
+            self.output_models[1].load_weights(weights[1])
+            if hasattr(self, 'target_actor'):
+                self.target_actor.load_weights(weights[0])
+            if hasattr(self, 'target_critic'):
+                self.target_critic.load_weights(weights[1])
+        elif isinstance(weights, str):
             self.model.load_weights(weights)
             if hasattr(self, 'target_model'):
                 self.target_model.load_weights(weights)
@@ -499,7 +527,6 @@ class OffPolicy(BaseAgent, ABC):
             for _ in range(self.n_envs)
         ]
         self.buffer_batch_size = buffer_batch_size
-        self.target_model = tf.keras.models.clone_model(self.model)
         self.epsilon_start = self.epsilon = epsilon_start
         self.epsilon_end = epsilon_end
         self.epsilon_decay_steps = epsilon_decay_steps
@@ -509,10 +536,6 @@ class OffPolicy(BaseAgent, ABC):
         self.epsilon = max(
             self.epsilon_end, self.epsilon_start - self.steps / self.epsilon_decay_steps
         )
-
-    def sync_target_model(self):
-        if self.steps % self.target_sync_steps == 0:
-            self.target_model.set_weights(self.model.get_weights())
 
     def fill_buffers(self):
         """
@@ -527,7 +550,7 @@ class OffPolicy(BaseAgent, ABC):
             buffer = self.buffers[i]
             state = self.states[i]
             while len(buffer) < buffer.initial_size:
-                action = np.random.randint(0, self.n_actions)
+                action = env.action_space.sample()
                 new_state, reward, done, _ = env.step(action)
                 buffer.append((state, action, reward, done, new_state))
                 state = new_state

dqn.py
@@ -1,7 +1,8 @@
 import numpy as np
 import tensorflow as tf
-from base_agents import OffPolicy
 from gym.spaces.discrete import Discrete
+
+from base_agents import OffPolicy
 
 
 class DQN(OffPolicy):
@@ -35,6 +36,7 @@ class DQN(OffPolicy):
             f'Invalid environment: {envs[0].spec.id}. DQN supports '
             f'environments with a discrete action space only, got {envs[0].action_space}'
         )
+        self.target_model = tf.keras.models.clone_model(self.model)
         self.double = double
         self.batch_indices = tf.range(
             self.buffer_batch_size * self.n_envs, dtype=tf.int64
@@ -56,6 +58,10 @@ class DQN(OffPolicy):
         """
         q_values = super(DQN, self).get_model_outputs(inputs, models, training)
         return tf.argmax(q_values, 1), q_values
+
+    def sync_target_model(self):
+        if self.steps % self.target_sync_steps == 0:
+            self.target_model.set_weights(self.model.get_weights())
 
     def get_actions(self):
         """

commit 571447d002a6290819eaf79ffcc41a10d464d5a8

Add the new replay buffer type which otherwise, TD3 won't work.
- Separate replay buffers and move them outside the given agent class, to be passed as a parameter.
- `buffer_max_size`, `buffer_initial_size`, and `buffer_batch_size` are no longer OffPolicy parameters, they should be specified upon buffer creation.
- Update `fill_buffers()` OffPolicy method to incorporate new changes.
- `buffers` is now a required parameter in off-policy agents
- Fix a bug in DQN introduced earlier in 21ebb9ec224d0afe71cd1a0e96b3966f11848638
- Update TD3 scratch

acer.py
@@ -1,7 +1,6 @@
 import numpy as np
 import tensorflow as tf
 from a2c import A2C
-from utils import ReplayBuffer
 
 
 class ACER(A2C):
@@ -9,9 +8,8 @@ class ACER(A2C):
         self,
         envs,
         model,
+        buffers,
         ema_alpha=0.99,
-        buffer_max_size=5000,
-        buffer_initial_size=500,
         replay_ratio=4,
         epsilon=1e-6,
         importance_c=10.0,
@@ -39,14 +37,7 @@ class ACER(A2C):
         super(ACER, self).__init__(envs, model, **kwargs)
         self.avg_model = tf.keras.models.clone_model(self.model)
         self.ema = tf.train.ExponentialMovingAverage(ema_alpha)
-        self.buffers = [
-            ReplayBuffer(
-                buffer_max_size // self.n_envs,
-                buffer_initial_size // self.n_envs,
-                batch_size=1,
-            )
-            for _ in range(self.n_envs)
-        ]
+        self.buffers = buffers
         self.batch_indices = tf.range(self.n_steps * self.n_envs, dtype=tf.int64)[
             :, tf.newaxis
         ]
@@ -140,7 +131,7 @@ class ACER(A2C):
             env_outputs = []
             for item in batch:
                 env_outputs.append(item[i])
-            self.buffers[i].append(env_outputs)
+            self.buffers[i].append(*env_outputs)
 
     def get_batch(self):
         """
@@ -361,7 +352,7 @@ class ACER(A2C):
         self.update_gradients(*batch)
         if (
             self.replay_ratio > 0
-            and len(self.buffers[0]) >= self.buffers[0].initial_size
+            and self.buffers[0].current_size >= self.buffers[0].initial_size
         ):
             for _ in range(np.random.poisson(self.replay_ratio)):
                 batch = tf.numpy_function(
@@ -373,7 +364,8 @@ class ACER(A2C):
 
 
 if __name__ == '__main__':
-    from utils import ModelReader, create_gym_env
+    from utils import (IAmTheOtherKindOfReplayBufferBecauseFuckTensorflow,
+                       ModelReader, ReplayBuffer, create_gym_env)
 
     seed = None
     es = create_gym_env('PongNoFrameskip-v4', 16, scale_frames=False)
@@ -386,5 +378,15 @@ if __name__ == '__main__':
         seed,
     )
     m = mh.build_model()
-    agn = ACER(es, m, seed=seed, scale_factor=255.0, n_steps=20, grad_norm=10)
+    bs = [
+        ReplayBuffer(5000 // len(es), initial_size=500 // len(es), batch_size=1)
+        for _ in range(len(es))
+    ]
+    # bs = [
+    #     IAmTheOtherKindOfReplayBufferBecauseFuckTensorflow(
+    #         5000 // len(es), 5, batch_size=1, initial_size=500 // len(es)
+    #     )
+    #     for _ in range(len(es))
+    # ]
+    agn = ACER(es, m, bs, seed=seed, scale_factor=255.0, n_steps=20, grad_norm=10)
     agn.fit(19)

base_agents.py
@@ -13,8 +13,6 @@ import wandb
 from gym.spaces.box import Box
 from gym.spaces.discrete import Discrete
 
-from utils import ReplayBuffer
-
 
 class BaseAgent(ABC):
     def __init__(
@@ -252,7 +250,7 @@ class BaseAgent(ABC):
             self.episode_rewards[i] += reward
             observation = state, action, reward, done, new_state
             if store_in_buffers and hasattr(self, 'buffers'):
-                self.buffers[i].append(observation)
+                self.buffers[i].append(*observation)
             if get_observation:
                 observations.append(observation)
             if done:
@@ -480,28 +478,19 @@ class OffPolicy(BaseAgent, ABC):
         self,
         envs,
         model,
+        buffers,
         epsilon_start=1.0,
         epsilon_end=0.02,
         epsilon_decay_steps=150000,
         target_sync_steps=1000,
-        buffer_max_size=10000,
-        buffer_initial_size=None,
-        buffer_batch_size=32,
         **kwargs,
     ):
         super(OffPolicy, self).__init__(envs, model, **kwargs)
-        buffer_initial_size = buffer_initial_size or buffer_max_size
-        self.buffers = [
-            ReplayBuffer(
-                buffer_max_size // self.n_envs,
-                buffer_initial_size // self.n_envs,
-                self.n_steps,
-                self.gamma,
-                buffer_batch_size,
-            )
-            for _ in range(self.n_envs)
-        ]
-        self.buffer_batch_size = buffer_batch_size
+        assert len(envs) == len(buffers), (
+            f'Expected equal env and replay buffer sizes, got {self.n_envs} '
+            f'and {len(buffers)}'
+        )
+        self.buffers = buffers
         self.epsilon_start = self.epsilon = epsilon_start
         self.epsilon_end = epsilon_end
         self.epsilon_decay_steps = epsilon_decay_steps
@@ -524,14 +513,14 @@ class OffPolicy(BaseAgent, ABC):
         for i, env in enumerate(self.envs):
             buffer = self.buffers[i]
             state = self.states[i]
-            while len(buffer) < buffer.initial_size:
+            while buffer.current_size < buffer.initial_size:
                 action = env.action_space.sample()
                 new_state, reward, done, _ = env.step(action)
-                buffer.append((state, action, reward, done, new_state))
+                buffer.append(state, action, reward, done, new_state)
                 state = new_state
                 if done:
                     state = env.reset()
-                sizes[i] = len(buffer)
+                sizes[i] = buffer.current_size
                 filled = sum(sizes.values())
                 complete = round((filled / total_size) * 100, self.display_precision)
                 print(

dqn.py
@@ -10,6 +10,7 @@ class DQN(OffPolicy):
         self,
         envs,
         model,
+        buffers,
         double=False,
         **kwargs,
     ):
@@ -31,7 +32,7 @@ class DQN(OffPolicy):
             epsilon_decay_steps: Decay epsilon for n steps.
             **kwargs: kwargs Passed to OnPolicy
         """
-        super(DQN, self).__init__(envs, model, **kwargs)
+        super(DQN, self).__init__(envs, model, buffers, **kwargs)
         assert isinstance(envs[0].action_space, Discrete), (
             f'Invalid environment: {envs[0].spec.id}. DQN supports '
             f'environments with a discrete action space only, got {envs[0].action_space}'
@@ -39,7 +40,7 @@ class DQN(OffPolicy):
         self.target_model = tf.keras.models.clone_model(self.model)
         self.double = double
         self.batch_indices = tf.range(
-            self.buffer_batch_size * self.n_envs, dtype=tf.int64
+            self.buffers[0].batch_size * self.n_envs, dtype=tf.int64
         )[:, tf.newaxis]
 
     @tf.function
@@ -149,7 +150,7 @@ class DQN(OffPolicy):
         Returns:
             None
         """
-        actions = self.get_actions()
+        actions = tf.numpy_function(self.get_actions, [], tf.int64)
         tf.numpy_function(self.step_envs, [actions, False, True], [])
         training_batch = tf.numpy_function(
             self.concat_buffer_samples,
@@ -171,18 +172,19 @@ class DQN(OffPolicy):
 
 
 if __name__ == '__main__':
-    from utils import ModelReader, create_gym_env
+    from utils import ModelReader, ReplayBuffer, create_gym_env
 
-    gym_envs = create_gym_env('PongNoFrameskip-v4', 3)
+    en = create_gym_env('PongNoFrameskip-v4', 3)
     seed = None
     from tensorflow.keras.optimizers import Adam
 
     optimizer = Adam(1e-4)
     mh = ModelReader(
-        'models/cnn/dqn.cfg', [6], gym_envs[0].observation_space.shape, optimizer, seed
+        'models/cnn/dqn.cfg', [6], en[0].observation_space.shape, optimizer, seed
     )
     m = mh.build_model()
-    agn = DQN(gym_envs, m, buffer_max_size=10000, seed=seed)
+    bs = [ReplayBuffer(10000 // len(en)) for _ in range(len(en))]
+    agn = DQN(en, m, bs, seed=seed)
     agn.fit(19)
     # agn.play(
     #     '/Users/emadboctor/Desktop/code/models-drl/dqn-pong-19-model/pong_test.tf',

td3.py
@@ -7,6 +7,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Dense, Input
+from utils import IAmTheOtherKindOfReplayBufferBecauseFuckTensorflow
 
 
 def actor(input_shape, output_units):
@@ -27,26 +28,6 @@ def critic(input_shape):
     model = Model(x0, ouput)
     model.call = tf.function(model.call)
     return model
-
-
-class IAmTheOtherKindOfReplayBufferBecauseFuckTensorflow:
-    def __init__(self, size, slots):
-        self.size = size
-        self.slots = [np.array([])] * slots
-        self.current_size = 0
-
-    def add(self, *args):
-        for i, arg in enumerate(args):
-            if not isinstance(arg, np.ndarray):
-                arg = np.array([arg])
-            if not self.slots[i].shape[0]:
-                self.slots[i] = np.zeros((self.size, *arg.shape), np.float32)
-            self.slots[i][self.current_size % self.size] = arg.copy()
-        self.current_size += 1
-
-    def sample(self, batch_size):
-        indices = np.random.randint(0, min(self.current_size, self.size), batch_size)
-        return [slot[indices] for slot in self.slots]
 
 
 class TD3:
@@ -121,7 +102,7 @@ class TD3:
         for model in (self.actor, self.critic1, self.critic2):
             model.compile('adam')
         self.replay_buffer = IAmTheOtherKindOfReplayBufferBecauseFuckTensorflow(
-            buffer_size, 5
+            buffer_size, 5, batch_size=batch_size, initial_size=learning_starts
         )
 
     def critic_loss(self, obs, action, next_obs, done, reward):
@@ -177,9 +158,9 @@ class TD3:
             zip(grads_actor, self.actor.trainable_variables)
         )
 
-    def train(self, gradient_steps, batch_size=100, policy_delay=2):
+    def train(self, gradient_steps, policy_delay=2):
         for gradient_step in range(gradient_steps):
-            obs, new_obs, action, reward, done = self.replay_buffer.sample(batch_size)
+            obs, new_obs, action, reward, done = self.replay_buffer.get_sample()
             self._train_critic(obs, action, new_obs, done, reward)
             if gradient_step % policy_delay == 0:
                 self._train_actor(obs)
@@ -198,14 +179,12 @@ class TD3:
                 self.env,
                 learning_starts=self.learning_starts,
                 num_timesteps=self.num_timesteps,
-                replay_buffer=self.replay_buffer,
                 obs=obs,
                 log_interval=log_interval,
             )
             if self.steps > 0 and self.steps > self.learning_starts:
                 self.train(
                     self.gradient_steps or self.last_episode_steps[0],
-                    batch_size=self.batch_size,
                     policy_delay=self.policy_delay,
                 )
         return self
@@ -215,7 +194,6 @@ class TD3:
         env,
         learning_starts=0,
         num_timesteps=0,
-        replay_buffer=None,
         obs=None,
         log_interval=1,
     ):
@@ -227,8 +205,7 @@ class TD3:
             new_obs, reward, done, infos = env.step(action)
             done_bool = float(done)
             self.episode_reward += reward
-            if replay_buffer is not None:
-                replay_buffer.add(obs, new_obs, action, reward, done_bool)
+            self.replay_buffer.append(obs, new_obs, action, reward, done_bool)
             obs = new_obs
             num_timesteps += 1
             self.steps += 1
@@ -276,6 +253,6 @@ class TD3:
 
 
 if __name__ == '__main__':
-    env = gym.make('BipedalWalker-v3')
-    agent = TD3(env)
+    en = gym.make('BipedalWalker-v3')
+    agent = TD3(en)
     agent.learn(1000000, log_interval=10)

utils.py
@@ -91,36 +91,43 @@ class AtariPreprocessor(gym.Wrapper):
         return self.process_frame(observation)
 
 
-class ReplayBuffer(deque):
+class BaseBuffer:
+    def __init__(self, size, initial_size=None, batch_size=32):
+        if initial_size:
+            assert size >= initial_size, 'Buffer initial size exceeds max size'
+        self.size = size
+        self.initial_size = initial_size or size
+        self.batch_size = batch_size
+        self.current_size = 0
+
+    def append(self, *args):
+        raise NotImplementedError(
+            f'append() should be implemented by {self.__class__.__name__} subclasses'
+        )
+
+    def get_sample(self):
+        raise NotImplementedError(
+            f'get_sample() should be implemented by {self.__class__.__name__} subclasses'
+        )
+
+
+class ReplayBuffer(BaseBuffer):
     """
     Replay buffer that holds state transitions
     """
 
-    def __init__(
-        self,
-        max_size,
-        initial_size=None,
-        n_steps=1,
-        gamma=0.99,
-        batch_size=32,
-    ):
+    def __init__(self, size, n_steps=1, gamma=0.99, **kwargs):
         """
         Initialize buffer settings.
         Args:
-            max_size: Maximum transitions to store.
-            initial_size: Maximum transitions to store before starting the training.
             n_steps: Steps separating start and end frames.
             gamma: Discount factor.
-            batch_size: Size of the sampling method batch.
         """
-        if initial_size:
-            assert max_size >= initial_size, 'Buffer initial size exceeds max size'
-        super(ReplayBuffer, self).__init__(maxlen=max_size)
-        self.initial_size = initial_size or max_size
+        super(ReplayBuffer, self).__init__(size, **kwargs)
         self.n_steps = n_steps
         self.gamma = gamma
+        self.main_buffer = deque(maxlen=size)
         self.temp_buffer = []
-        self.batch_size = batch_size
 
     def reset_temp_history(self):
         """
@@ -140,24 +147,25 @@ class ReplayBuffer(deque):
         self.temp_buffer.clear()
         return state, action, reward, done, new_state
 
-    def append(self, experience):
+    def append(self, *args):
         """
         Append experience and auto-allocate to temp buffer / main buffer(self)
         Args:
-            experience: A list of observed values ex: (state, reward, done, new state ...)
+            *args: Items to store
 
         Returns:
             None
         """
+        self.current_size += 1
         if self.n_steps == 1:
-            super(ReplayBuffer, self).append(experience)
+            self.main_buffer.append(args)
             return
         if (self.temp_buffer and self.temp_buffer[-1][3]) or len(
             self.temp_buffer
         ) == self.n_steps:
             adjusted_sample = self.reset_temp_history()
-            super(ReplayBuffer, self).append(adjusted_sample)
-        self.temp_buffer.append(experience)
+            self.main_buffer.append(adjusted_sample)
+        self.temp_buffer.append(args)
 
     def get_sample(self):
         """
@@ -167,10 +175,34 @@ class ReplayBuffer(deque):
             A batch of observations that has the same length as the
                 previously appended experience.
         """
-        memories = random.sample(self, self.batch_size)
+        memories = random.sample(self.main_buffer, self.batch_size)
         if self.batch_size > 1:
             return [np.array(item) for item in zip(*memories)]
         return memories[0]
+
+
+class IAmTheOtherKindOfReplayBufferBecauseFuckTensorflow(BaseBuffer):
+    def __init__(self, size, slots, **kwargs):
+        super(IAmTheOtherKindOfReplayBufferBecauseFuckTensorflow, self).__init__(
+            size, **kwargs
+        )
+        self.slots = [np.array([])] * slots
+        self.current_size = 0
+
+    def append(self, *args):
+        self.current_size += 1
+        for i, arg in enumerate(args):
+            if not isinstance(arg, np.ndarray):
+                arg = np.array([arg])
+            if not self.slots[i].shape[0]:
+                self.slots[i] = np.zeros((self.size, *arg.shape), np.float32)
+            self.slots[i][self.current_size % self.size] = arg.copy()
+
+    def get_sample(self):
+        indices = np.random.randint(
+            0, min(self.current_size, self.size), self.batch_size
+        )
+        return [slot[indices] for slot in self.slots]
 
 
 def create_gym_env(env_name, n=1, preprocess=True, *args, **kwargs):