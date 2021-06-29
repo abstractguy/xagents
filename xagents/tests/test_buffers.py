abstractguy/xagents

commit a01f2f25446bfd9aebe71e5a964477cf14a9267b

Automate X server start and update docstrings.
- Add `xhost +` invocation to prevent errors given by pyvirtualdisplay.
- Remove virtualize_display decorator, to be replaced by `pytest-xvfb` plugin which used to abruptly stop pytest on Ubuntu18.
- Add `get_wandb_key()` to skip respective tests if no existing configuration is found.
- Add `box2d`, `pytest-xvfb`, and gym[atari] to requirements.txt
- Update docstrings.

xagents/base.py
@@ -13,8 +13,6 @@ import wandb
 from gym.spaces.box import Box
 from gym.spaces.discrete import Discrete
 
-from xagents.utils.common import virtualize_display
-
 
 class BaseAgent(ABC):
     def __init__(
@@ -90,6 +88,14 @@ class BaseAgent(ABC):
         self.set_action_count()
 
     def set_seeds(self, seed):
+        """
+        Set random seeds for numpy, tensorflow, random, gym
+        Args:
+            seed: int, random seed.
+
+        Returns:
+            None
+        """
         tf.random.set_seed(seed)
         np.random.seed(seed)
         for env in self.envs:
@@ -436,7 +442,6 @@ class BaseAgent(ABC):
             self.train_step()
             self.at_step_end()
 
-    @virtualize_display
     def play(
         self,
         video_dir=None,

xagents/cli.py
@@ -110,20 +110,22 @@ class Executor:
                     f'--{arg}', help=_help, default=_default, action=_action
                 )
 
-    def maybe_create_agent(self, cli_args):
+    def maybe_create_agent(self, argv):
         """
         Display help respective to parsed commands or set self.agent_id and self.command
         for further execution if enough arguments are given.
+        Args:
+            argv: Arguments passed through sys.argv or otherwise.
 
         Returns:
             None
         """
         to_display = {}
-        total = len(cli_args)
+        total = len(argv)
         if total == 0:
             self.display_commands()
             return
-        command = cli_args[0]
+        command = argv[0]
         to_display.update(non_agent_args)
         to_display.update(agent_args)
         assert command in xagents.commands, f'Invalid command `{command}`'
@@ -131,9 +133,9 @@ class Executor:
         if total == 1:
             self.display_commands({command: to_display})
             return
-        agent_id = cli_args[1]
+        agent_id = argv[1]
         assert agent_id in xagents.agents, f'Invalid agent `{agent_id}`'
-        to_display.update(xagents.agents[agent_id]['module'].cli_args)
+        to_display.update(xagents.agents[agent_id]['module'].argv)
         if total == 2:
             title = f'{command} {agent_id}'
             if (
@@ -145,9 +147,11 @@ class Executor:
             return
         self.command, self.agent_id = command, agent_id
 
-    def parse_known_args(self, cli_args):
+    def parse_known_args(self, argv):
         """
         Parse general, agent and command specific args.
+        Args:
+            argv: Arguments passed through sys.argv or otherwise.
 
         Returns:
             agent kwargs, non-agent kwargs and command kwargs.
@@ -156,7 +160,7 @@ class Executor:
         agent_parser = argparse.ArgumentParser()
         command_parser = argparse.ArgumentParser()
         self.add_args(agent_args, agent_parser)
-        self.add_args(xagents.agents[self.agent_id]['module'].cli_args, agent_parser)
+        self.add_args(xagents.agents[self.agent_id]['module'].argv, agent_parser)
         self.add_args(xagents.commands[self.command][0], command_parser)
         if (
             issubclass(xagents.agents[self.agent_id]['agent'], OffPolicy)
@@ -164,9 +168,9 @@ class Executor:
         ):
             self.add_args(off_policy_args, general_parser)
         self.add_args(non_agent_args, general_parser)
-        non_agent_known = general_parser.parse_known_args(cli_args)[0]
-        agent_known = agent_parser.parse_known_args(cli_args)[0]
-        command_known = command_parser.parse_known_args(cli_args)[0]
+        non_agent_known = general_parser.parse_known_args(argv)[0]
+        agent_known = agent_parser.parse_known_args(argv)[0]
+        command_known = command_parser.parse_known_args(argv)[0]
         if (
             self.command == 'train'
             and command_known.target_reward is None
@@ -317,18 +321,20 @@ class Executor:
             ]
         return buffers
 
-    def execute(self, cli_args=None):
+    def execute(self, argv):
         """
         Parse command line arguments, display help or execute command
         if enough arguments are given.
+        Args:
+            argv: Arguments passed through sys.argv or otherwise.
 
         Returns:
             None
         """
-        self.maybe_create_agent(cli_args)
+        self.maybe_create_agent(argv)
         if not self.agent_id:
             return
-        agent_known, non_agent_known, command_known = self.parse_known_args(cli_args)
+        agent_known, non_agent_known, command_known = self.parse_known_args(argv)
         envs = create_gym_env(
             non_agent_known.env,
             non_agent_known.n_envs,
@@ -359,9 +365,18 @@ class Executor:
         getattr(self.agent, xagents.commands[self.command][1])(**command_known)
 
 
-def execute(cli_args=None):
-    cli_args = cli_args or sys.argv[1:]
-    Executor().execute(cli_args)
+def execute(argv=None):
+    """
+    Parse and execute commands.
+    Args:
+        argv: List of arguments to be passed to Executor.execute()
+            if not specified, defaults. to sys.argv[1:]
+
+    Returns:
+        None
+    """
+    argv = argv or sys.argv[1:]
+    Executor().execute(argv)
 
 
 if __name__ == '__main__':

xagents/tests/__init__.py
@@ -1,0 +1,3 @@
+import subprocess
+
+subprocess.run(['xhost', '+'])

xagents/tests/conftest.py
@@ -13,6 +13,14 @@ from xagents.utils.cli import agent_args, non_agent_args, play_args, train_args
 
 @pytest.fixture(scope='function')
 def executor(request):
+    """
+    Fixture for testing command line options.
+    Args:
+        request: _pytest.fixtures.SubRequest
+
+    Returns:
+        xagents.cli.Executor
+    """
     request.cls.executor = Executor()
 
 
@@ -26,21 +34,58 @@ def executor(request):
     ]
 )
 def section(request):
+    """
+    Fixture for testing help menu and parsing sanity.
+    Args:
+        request: _pytest.fixtures.SubRequest
+
+    Yields:
+        Tuple of argument group name and respective dict of args.
+    """
     yield request.param
 
 
 @pytest.fixture(scope='class')
 def envs(request):
+    """
+    Fixture of PongNoFrameskip-v4 environments used to test agents that support
+    environments with Discrete action space.
+    Args:
+        request: _pytest.fixtures.SubRequest
+
+    Returns:
+        A list of 4 pong environments.
+    """
     request.cls.envs = [gym.make('PongNoFrameskip-v4') for _ in range(4)]
 
 
 @pytest.fixture(scope='class')
 def envs2(request):
+    """
+    Fixture of BipedalWalker-v3 environments used to test agents that support
+    environments with continuous action space.
+    Args:
+        request: _pytest.fixtures.SubRequest
+
+    Returns:
+        A list of 4 bipedal walker environments.
+    """
     request.cls.envs2 = [gym.make('BipedalWalker-v3') for _ in range(4)]
 
 
 @pytest.fixture(scope='class')
 def model(request):
+    """
+    Fixture used by all agents in tests that require a model but do not
+    require the agent default model. It should be included in
+    pytest.mark.usefixtures() decorator to allow its availability as
+    a class attribute.
+    Args:
+        request: _pytest.fixtures.SubRequest
+
+    Returns:
+        None
+    """
     x0 = Input((210, 160, 3))
     x = Dense(1, 'relu')(x0)
     x = Dense(1, 'relu')(x)
@@ -51,29 +96,81 @@ def model(request):
 
 @pytest.fixture(scope='class')
 def buffers(request):
+    """
+    Fixture used by off-policy agent tests that require a replay buffer
+    that is not size specific. It should be included in
+    pytest.mark.usefixtures() decorator to allow its availability as
+    a class attribute.
+    Args:
+        request: _pytest.fixtures.SubRequest
+
+    Returns:
+        None
+    """
     request.cls.buffers = [ReplayBuffer(10, batch_size=155) for _ in range(4)]
 
 
 @pytest.fixture(params=[item['agent'] for item in xagents.agents.values()])
 def agent(request):
+    """
+    Fixture with agents available in xagents.agents
+    Args:
+        request: _pytest.fixtures.SubRequest
+
+    Yields:
+        OnPolicy/OffPolicy subclass.
+    """
     yield request.param
 
 
 @pytest.fixture(params=[agent_id for agent_id in xagents.agents])
 def agent_id(request):
+    """
+    Fixture with agent ids available in xagents.agents
+    Args:
+        request: _pytest.fixtures.SubRequest
+
+    Yields:
+        Agent id as str
+    """
     yield request.param
 
 
 @pytest.fixture(params=[command for command in xagents.commands])
 def command(request):
+    """
+    Fixture with commands available in xagents.commands
+    Args:
+        request: _pytest.fixtures.SubRequest
+
+    Yields:
+        Command as str
+    """
     yield request.param
 
 
 @pytest.fixture(params=[ACER, TD3, DQN, DDPG])
 def off_policy_agent(request):
+    """
+    Fixture with off-policy agents.
+    Args:
+        request: _pytest.fixtures.SubRequest
+
+    Yields:
+        OffPolicy subclass.
+    """
     yield request.param
 
 
 @pytest.fixture(params=[BaseAgent, OnPolicy, OffPolicy])
 def base_agent(request):
+    """
+    Fixture with agent base classes, used to test base and
+    abstract methods.
+    Args:
+        request: _pytest.fixtures.SubRequest
+
+    Yields:
+        OnPolicy/OffPolicy subclass.
+    """
     yield request.param

commit 5547af08614a92a2110c1cd9ac25e43394ca2a1f

Add buffer tests, and fix minor bugs.
- Restrict `xhost +` invocation to macos.
- Increment self.steps in agent.fit() instead of agent.step_envs()
- Fix a bug introduced in a01f2f25446bfd9aebe71e5a964477cf14a9267b that changed agent `cli_args` attribute to `argv` unintentionally.
- Add tests for `fit()`, `at_step_start()`, `at_step_end()`, and buffers.

xagents/base.py
@@ -283,7 +283,6 @@ class BaseAgent(ABC):
             new_state, reward, done, _ = env.step(action)
             self.states[i] = new_state
             self.dones[i] = done
-            self.steps += 1
             self.episode_rewards[i] += reward
             observation = state, action, reward, done, new_state
             if store_in_buffers and hasattr(self, 'buffers'):
@@ -441,6 +440,7 @@ class BaseAgent(ABC):
             self.at_step_start()
             self.train_step()
             self.at_step_end()
+            self.steps += self.n_envs
 
     def play(
         self,

xagents/cli.py
@@ -8,8 +8,7 @@ from tensorflow.keras.optimizers import Adam
 
 import xagents
 from xagents.base import OffPolicy
-from xagents.utils.buffers import (
-    IAmTheOtherKindOfReplayBufferBecauseFuckTensorflow, ReplayBuffer)
+from xagents.utils.buffers import ReplayBuffer1, ReplayBuffer2
 from xagents.utils.cli import agent_args, non_agent_args, off_policy_args
 from xagents.utils.common import ModelReader, create_gym_env
 
@@ -135,7 +134,7 @@ class Executor:
             return
         agent_id = argv[1]
         assert agent_id in xagents.agents, f'Invalid agent `{agent_id}`'
-        to_display.update(xagents.agents[agent_id]['module'].argv)
+        to_display.update(xagents.agents[agent_id]['module'].cli_args)
         if total == 2:
             title = f'{command} {agent_id}'
             if (
@@ -160,7 +159,7 @@ class Executor:
         agent_parser = argparse.ArgumentParser()
         command_parser = argparse.ArgumentParser()
         self.add_args(agent_args, agent_parser)
-        self.add_args(xagents.agents[self.agent_id]['module'].argv, agent_parser)
+        self.add_args(xagents.agents[self.agent_id]['module'].cli_args, agent_parser)
         self.add_args(xagents.commands[self.command][0], command_parser)
         if (
             issubclass(xagents.agents[self.agent_id]['agent'], OffPolicy)
@@ -300,7 +299,7 @@ class Executor:
             buffer_batch_size = 1
         if self.agent_id in ['td3', 'ddpg']:
             buffers = agent_known_args['buffers'] = [
-                IAmTheOtherKindOfReplayBufferBecauseFuckTensorflow(
+                ReplayBuffer2(
                     buffer_max_size,
                     5,
                     initial_size=buffer_initial_size,
@@ -310,7 +309,7 @@ class Executor:
             ]
         else:
             buffers = agent_known_args['buffers'] = [
-                ReplayBuffer(
+                ReplayBuffer1(
                     buffer_max_size,
                     non_agent_known_args.buffer_n_steps,
                     agent_known_args['gamma'],

xagents/utils/buffers.py
@@ -23,6 +23,9 @@ class BaseBuffer:
         ), f'Buffer initial size should be > 0, got {initial_size}'
         assert size > 0, f'Buffer size should be > 0,  got {size}'
         assert batch_size > 0, f'Buffer batch size should be > 0, got {batch_size}'
+        assert (
+            batch_size <= size
+        ), f'Buffer batch size `{batch_size}` should be <= size `{size}`'
         if initial_size:
             assert size >= initial_size, 'Buffer initial size exceeds max size'
         self.size = size
@@ -53,7 +56,7 @@ class BaseBuffer:
         )
 
 
-class ReplayBuffer(BaseBuffer):
+class ReplayBuffer1(BaseBuffer):
     """
     deque-based replay buffer that holds state transitions
     """
@@ -67,7 +70,7 @@ class ReplayBuffer(BaseBuffer):
             gamma: Discount factor.
             **kwargs: kwargs passed to BaseBuffer.
         """
-        super(ReplayBuffer, self).__init__(size, **kwargs)
+        super(ReplayBuffer1, self).__init__(size, **kwargs)
         self.n_steps = n_steps
         self.gamma = gamma
         self.main_buffer = deque(maxlen=size)
@@ -100,7 +103,7 @@ class ReplayBuffer(BaseBuffer):
         Returns:
             None
         """
-        self.current_size += 1
+        self.current_size = len(self.main_buffer)
         if self.n_steps == 1:
             self.main_buffer.append(args)
             return
@@ -125,7 +128,7 @@ class ReplayBuffer(BaseBuffer):
         return memories[0]
 
 
-class IAmTheOtherKindOfReplayBufferBecauseFuckTensorflow(BaseBuffer):
+class ReplayBuffer2(BaseBuffer):
     """
     numpy-based replay buffer added for compatibility with tensorflow shortcomings.
     """
@@ -139,9 +142,7 @@ class IAmTheOtherKindOfReplayBufferBecauseFuckTensorflow(BaseBuffer):
             slots: Number of args that will be passed to self.append()
             **kwargs: kwargs passed to BaseBuffer.
         """
-        super(IAmTheOtherKindOfReplayBufferBecauseFuckTensorflow, self).__init__(
-            size, **kwargs
-        )
+        super(ReplayBuffer2, self).__init__(size, **kwargs)
         self.slots = [np.array([])] * slots
         self.current_size = 0
 
@@ -154,13 +155,14 @@ class IAmTheOtherKindOfReplayBufferBecauseFuckTensorflow(BaseBuffer):
         Returns:
             None
         """
-        self.current_size += 1
         for i, arg in enumerate(args):
             if not isinstance(arg, np.ndarray):
                 arg = np.array([arg])
             if not self.slots[i].shape[0]:
                 self.slots[i] = np.zeros((self.size, *arg.shape), np.float32)
             self.slots[i][self.current_size % self.size] = arg.copy()
+        if self.current_size < self.size:
+            self.current_size += 1
 
     def get_sample(self):
         """

xagents/tests/__init__.py
@@ -1,3 +1,5 @@
 import subprocess
+import sys
 
-subprocess.run(['xhost', '+'])
+if sys.platform == 'darwin':
+    subprocess.run(['xhost', '+'])

xagents/tests/conftest.py
@@ -1,4 +1,5 @@
 import gym
+import numpy as np
 import pytest
 from tensorflow.keras.layers import Dense, Input
 from tensorflow.keras.models import Model
@@ -7,7 +8,7 @@ import xagents
 from xagents import ACER, DDPG, DQN, TD3
 from xagents.base import BaseAgent, OffPolicy, OnPolicy
 from xagents.cli import Executor
-from xagents.utils.buffers import ReplayBuffer
+from xagents.utils.buffers import ReplayBuffer1, ReplayBuffer2
 from xagents.utils.cli import agent_args, non_agent_args, play_args, train_args
 
 
@@ -107,7 +108,33 @@ def buffers(request):
     Returns:
         None
     """
-    request.cls.buffers = [ReplayBuffer(10, batch_size=155) for _ in range(4)]
+    request.cls.buffers = [ReplayBuffer1(200, batch_size=155) for _ in range(4)]
+
+
+@pytest.fixture(scope='function')
+def buffer1(request):
+    """
+    Fixture used to test ReplayBuffer1.
+    Args:
+        request: _pytest.fixtures.SubRequest
+
+    Returns:
+        None
+    """
+    request.cls.buffer = ReplayBuffer1(100, batch_size=4)
+
+
+@pytest.fixture(scope='function')
+def buffer2(request):
+    """
+    Fixture used to test ReplayBuffer2.
+    Args:
+        request: _pytest.fixtures.SubRequest
+
+    Returns:
+        None
+    """
+    request.cls.buffer = ReplayBuffer2(100, 5, batch_size=4)
 
 
 @pytest.fixture(params=[item['agent'] for item in xagents.agents.values()])
@@ -174,3 +201,20 @@ def base_agent(request):
         OnPolicy/OffPolicy subclass.
     """
     yield request.param
+
+
+@pytest.fixture(params=[ReplayBuffer1, ReplayBuffer2])
+def buffer_type(request):
+    yield request.param
+
+
+@pytest.fixture(scope='class')
+def observations(request):
+    states = np.random.randint(0, 255, (10, 84, 84, 1))
+    actions = np.random.randint(0, 6, 10)
+    rewards = np.random.randint(0, 10, 10)
+    dones = np.random.randint(0, 1000000, 10)
+    new_states = np.random.randint(0, 255, (10, 84, 84, 1))
+    request.cls.observations = [
+        [*items] for items in zip(states, actions, rewards, dones, new_states)
+    ]

xagents/tests/test_base.py
@@ -11,8 +11,22 @@ from gym.spaces import Discrete
 import xagents
 from xagents import A2C, ACER, DDPG, DQN, PPO, TD3, TRPO
 from xagents.base import BaseAgent, OffPolicy, OnPolicy
-from xagents.utils.buffers import ReplayBuffer
+from xagents.utils.buffers import ReplayBuffer1
 from xagents.utils.common import get_wandb_key
+
+
+class DummyAgent(BaseAgent):
+    def __init__(self, envs, model, **kwargs):
+        super(DummyAgent, self).__init__(envs, model, **kwargs)
+
+    def at_step_start(self):
+        print(f'step starting')
+
+    def train_step(self):
+        print(f'train_step')
+
+    def at_step_end(self):
+        print(f'step ending')
 
 
 @pytest.mark.usefixtures('envs', 'envs2', 'model', 'buffers', 'executor')
@@ -187,10 +201,9 @@ class TestBase:
         assert 'given output models, got' in pe.value.args[0]
 
     @staticmethod
-    def check_progress_displayed(displayed):
+    def assert_progress_displayed(displayed):
         """
-        Check text displayed in the console and ensure it has
-        the expected keywords.
+        Assert text displayed to the console has the expected keywords.
         Args:
             displayed: str
         """
@@ -216,7 +229,7 @@ class TestBase:
         agent.frame_speed = agent.mean_reward = agent.best_reward = 0
         agent.display_metrics()
         displayed = capsys.readouterr().out
-        self.check_progress_displayed(displayed)
+        self.assert_progress_displayed(displayed)
 
     def test_update_metrics(self, agent):
         """
@@ -249,7 +262,7 @@ class TestBase:
         assert not capsys.readouterr().out
         agent.done_envs.extend(len(self.envs) * [1])
         agent.check_episodes()
-        self.check_progress_displayed(capsys.readouterr().out)
+        self.assert_progress_displayed(capsys.readouterr().out)
         assert not agent.done_envs
 
     @pytest.mark.parametrize(
@@ -298,7 +311,7 @@ class TestBase:
         Args:
             off_policy_agent: OffPolicy subclass.
         """
-        buffers = [ReplayBuffer(10, batch_size=1) for _ in range(4)]
+        buffers = [ReplayBuffer1(10, batch_size=1) for _ in range(4)]
         agent = off_policy_agent(
             **self.get_agent_kwargs(off_policy_agent, buffers=buffers)
         )
@@ -478,3 +491,15 @@ class TestBase:
             assert expected_frame.exists()
         assert len([*video_dir.rglob('openai*.mp4')])
         assert 'Maximum steps' in capsys.readouterr().out
+
+    def test_fit(self, capsys):
+        agent = DummyAgent(self.envs, self.model)
+        agent.fit(max_steps=1)
+        displayed = capsys.readouterr().out
+        assert agent.steps == 4
+        assert displayed.strip().split('\n') == [
+            'step starting',
+            'train_step',
+            'step ending',
+            'Maximum steps exceeded',
+        ]

xagents/tests/test_buffers.py
@@ -1,0 +1,118 @@
+import numpy as np
+import pytest
+
+from xagents.utils.buffers import BaseBuffer, ReplayBuffer2
+
+
+def get_buffer_kwargs(size, buffer_type, initial_size, batch_size):
+    buffer_kwargs = {
+        'size': size,
+        'initial_size': initial_size,
+        'batch_size': batch_size,
+    }
+    if buffer_type == ReplayBuffer2:
+        buffer_kwargs['slots'] = 1
+    return buffer_kwargs
+
+
+def assert_sample_shapes_match(buffer, shapes):
+    sample = buffer.get_sample()
+    size = sample[0].shape[0]
+    for shape, subsample in zip(shapes, sample):
+        assert np.squeeze(subsample).shape == (size, *shape)
+
+
+class TestBaseBuffer:
+    @pytest.mark.parametrize(
+        'size, initial_size, batch_size, exception, exception_kw',
+        [
+            [100, 0, 32, AssertionError, 'Buffer initial size should be > 0, got'],
+            [-100, 100, 32, AssertionError, 'Buffer size should be > 0'],
+            [10, None, 32, AssertionError, 'should be <= size'],
+            [100, 200, 32, AssertionError, 'Buffer initial size exceeds max size'],
+            [100, 50, 32, None, None],
+            [100, None, 32, None, None],
+        ],
+    )
+    def test_sizes(
+        self, buffer_type, size, initial_size, batch_size, exception, exception_kw
+    ):
+        buffer_kwargs = get_buffer_kwargs(size, buffer_type, initial_size, batch_size)
+        if exception:
+            with pytest.raises(exception) as pe:
+                buffer_type(**buffer_kwargs)
+            if exception_kw:
+                assert exception_kw in pe.value.args[0]
+        else:
+            buffer = buffer_type(**buffer_kwargs)
+            assert buffer.size == size
+            assert (
+                buffer.initial_size == buffer.size if not initial_size else initial_size
+            )
+            assert buffer.batch_size == batch_size
+
+    def test_abstract_methods(self):
+        buffer = BaseBuffer(32)
+        with pytest.raises(NotImplementedError) as pe1:
+            buffer.append(1)
+        with pytest.raises(NotImplementedError) as pe2:
+            buffer.get_sample()
+        for info in (pe1, pe2):
+            assert 'should be implemented' in info.value.args[0]
+
+
+@pytest.mark.usefixtures('buffer1', 'observations')
+class TestBuffer1:
+    def test_reset_temp_history(self):
+        self.buffer.temp_buffer.extend(self.observations)
+        state, action, reward, done, new_state = self.buffer.reset_temp_history()
+        assert (state == self.observations[0][0]).all()
+        assert action == self.observations[0][1]
+        assert done == self.observations[-1][3]
+        assert (new_state == self.observations[-1][-1]).all()
+        assert not self.buffer.temp_buffer
+
+    @pytest.mark.parametrize(
+        'args',
+        [
+            [1, 2],
+            [1, 2, 3, 4],
+            [np.random.randint(0, 1000, 10) for _ in range(5)],
+            [np.random.randint(0, 1000, 10), 15, 11],
+            [[np.random.randint(0, 1000, 10) for _ in range(5)]],
+        ],
+    )
+    def test_append(self, args):
+        self.buffer.append(*args)
+        for arg, saved in zip(args, self.buffer.main_buffer[0]):
+            if isinstance(arg, np.ndarray):
+                assert (arg == saved).all()
+            else:
+                assert arg == saved
+
+    def test_get_sample(self):
+        shapes = [np.squeeze(item).shape for item in self.observations[0]]
+        self.buffer.main_buffer.extend(self.observations)
+        assert_sample_shapes_match(self.buffer, shapes)
+
+
+@pytest.mark.usefixtures('observations', 'buffer2')
+class TestBuffer2:
+    def test_append(self):
+        expected_results = [[] for _ in range(5)]
+        for observation in self.observations:
+            self.buffer.append(*observation)
+            for item, expected_items in zip(observation, expected_results):
+                if not isinstance(item, np.ndarray):
+                    expected_items.append([item])
+                else:
+                    expected_items.append(item)
+        size = len(self.observations)
+        for expected, actual in zip(expected_results, self.buffer.slots):
+            assert (np.array(expected) == actual[:size]).all()
+
+    def test_get_sample(self):
+        shapes = [np.squeeze(item).shape for item in self.observations[0]]
+        for observation in self.observations:
+            self.buffer.append(*observation)
+            assert_sample_shapes_match(self.buffer, shapes)