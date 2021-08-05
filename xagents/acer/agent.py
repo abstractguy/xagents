abstractguy/xagents

commit c9b3addb7f38bf69affb418f4fb437bdbe589334

Fix issue with concurrent tuning by replacing create to load study instead, and do minor cleanups.
- Concurrent trials are now run after the creation of a study and the respective db, which is loaded by each trial.
- Add environment type checking to single type agents (ACER, DDPG, TD3, and DQN)


Former-commit-id: 1e13bba9c5c60a2457d0fc7ab546cc0e2622b46c

xagents/acer/agent.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+from gym.spaces import Discrete
 
 from xagents import A2C
 from xagents.utils.common import LazyFrames
@@ -35,6 +36,7 @@ class ACER(A2C):
             trust_region: If False, no trust region updates will be used.
             **kwargs: kwargs Passed to super classes.
         """
+        self.assert_valid_env(envs[0], Discrete)
         super(ACER, self).__init__(envs, model, **kwargs)
         self.avg_model = tf.keras.models.clone_model(self.model)
         self.ema = tf.train.ExponentialMovingAverage(ema_alpha)

xagents/ddpg/agent.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from gym.spaces import Box
 from tensorflow.keras.losses import MSE
 
 from xagents.base import OffPolicy
@@ -31,6 +32,7 @@ class DDPG(OffPolicy):
             policy_noise_coef: Coefficient multiplied by noise added to target actions.
             **kwargs: kwargs passed to super classes.
         """
+        self.assert_valid_env(envs[0], Box)
         super(DDPG, self).__init__(envs, actor_model, buffers, **kwargs)
         self.actor = actor_model
         self.critic = critic_model

xagents/dqn/agent.py
@@ -36,11 +36,8 @@ class DQN(OffPolicy):
             target_sync_steps: Steps to sync target models after each.
             **kwargs: kwargs passed to super classes.
         """
+        self.assert_valid_env(envs[0], Discrete)
         super(DQN, self).__init__(envs, model, buffers, **kwargs)
-        assert isinstance(envs[0].action_space, Discrete), (
-            f'Invalid environment: {envs[0].spec.id}. DQN supports '
-            f'environments with a discrete action space only, got {envs[0].action_space}'
-        )
         self.target_model = tf.keras.models.clone_model(self.model)
         self.target_model.set_weights(self.model.get_weights())
         self.double = double

xagents/td3/agent.py
@@ -57,7 +57,7 @@ class TD3(DDPG):
         Returns:
             actions
         """
-        return self.actor(tf.numpy_function(self.get_states, [], tf.float64))
+        return self.actor(tf.numpy_function(self.get_states, [], self.states[0].dtype))
 
     def update_critic_weights(self, states, actions, new_states, dones, rewards):
         """

xagents/base.py
@@ -123,6 +123,12 @@ class BaseAgent(ABC):
             'best reward',
         )
 
+    def assert_valid_env(self, env, valid_type):
+        assert isinstance(env.action_space, valid_type), (
+            f'Invalid environment: {env.spec.id}. {self.__class__.__name__} supports '
+            f'environments with a {valid_type} action space only, got {env.action_space}'
+        )
+
     def display_message(self, *args, **kwargs):
         """
         Display messages to the console.

xagents/utils/common.py
@@ -10,7 +10,7 @@ import numpy as np
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
-from gym.spaces import Discrete
+from gym.spaces import Box, Discrete
 from matplotlib import pyplot as plt
 from tensorflow.keras.initializers import GlorotUniform, Orthogonal
 from tensorflow.keras.layers import Conv2D, Dense, Flatten, Input
@@ -459,6 +459,10 @@ def create_model(
         seed,
     )
     if agent_id in ['td3', 'ddpg'] and 'critic' in model_cfg:
+        assert isinstance(env.action_space, Box), (
+            f'Invalid environment: {env.spec.id}. {agent_id.upper()} supports '
+            f'environments with a Box action space only, got {env.action_space}'
+        )
         model_reader.input_shape = (
             model_reader.input_shape[0] + env.action_space.shape[0]
         )

xagents/utils/tuning.py
@@ -108,20 +108,13 @@ def run_trial(
     if not command_known_args.non_silent:
         tf.get_logger().setLevel('ERROR')
         os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-        optuna.logging.set_verbosity(optuna.logging.ERROR)
         agent_known_args.quiet = True
-    pruner = optuna.pruners.MedianPruner(command_known_args.warmup_trials)
-    study = optuna.create_study(
-        study_name=command_known_args.study,
-        storage=command_known_args.storage,
-        load_if_exists=True,
-        direction='maximize',
-        pruner=pruner,
+    study = optuna.load_study(
+        command_known_args.study, storage=command_known_args.storage
     )
     objective = Objective(
         agent_id, agent_known_args, non_agent_known_args, command_known_args
     )
-    optuna.logging.set_verbosity(optuna.logging.INFO)
     study.optimize(objective, n_trials=1)
 
 
@@ -144,6 +137,14 @@ def run_tuning(agent_id, agent_known_args, non_agent_known_args, command_known_a
         'non_agent_known_args': non_agent_known_args,
         'command_known_args': command_known_args,
     }
+    pruner = optuna.pruners.MedianPruner(command_known_args.warmup_trials)
+    optuna.create_study(
+        study_name=command_known_args.study,
+        storage=command_known_args.storage,
+        load_if_exists=True,
+        direction='maximize',
+        pruner=pruner,
+    )
     for _ in range(command_known_args.n_trials // command_known_args.n_jobs):
         with ProcessPoolExecutor(command_known_args.n_jobs) as executor:
             future_trials = [

commit 6957af7429060ada58e4ec5aec4557c83e1bd0ab

Fix bugs caused by env type checking introduced in c9b3addb7f38bf69affb418f4fb437bdbe589334 [formerly 1e13bba9c5c60a2457d0fc7ab546cc0e2622b46c].
- Remove pyvirtualdisplay, and pytest-xvfb plugin from dependencies.
- Remove X server initialization in macOS tests.
- Fix failing tests, and update modules accordingly
- Update README.md


Former-commit-id: a82b7e4a3cc2d47dbb772568f3ff06b39b814186

xagents/acer/agent.py
@@ -36,8 +36,8 @@ class ACER(A2C):
             trust_region: If False, no trust region updates will be used.
             **kwargs: kwargs Passed to super classes.
         """
-        self.assert_valid_env(envs[0], Discrete)
         super(ACER, self).__init__(envs, model, **kwargs)
+        self.assert_valid_env(envs[0], Discrete)
         self.avg_model = tf.keras.models.clone_model(self.model)
         self.ema = tf.train.ExponentialMovingAverage(ema_alpha)
         self.buffers = buffers

xagents/ddpg/agent.py
@@ -32,8 +32,8 @@ class DDPG(OffPolicy):
             policy_noise_coef: Coefficient multiplied by noise added to target actions.
             **kwargs: kwargs passed to super classes.
         """
-        self.assert_valid_env(envs[0], Box)
         super(DDPG, self).__init__(envs, actor_model, buffers, **kwargs)
+        self.assert_valid_env(envs[0], Box)
         self.actor = actor_model
         self.critic = critic_model
         self.policy_delay = 1

xagents/dqn/agent.py
@@ -36,8 +36,8 @@ class DQN(OffPolicy):
             target_sync_steps: Steps to sync target models after each.
             **kwargs: kwargs passed to super classes.
         """
-        self.assert_valid_env(envs[0], Discrete)
         super(DQN, self).__init__(envs, model, buffers, **kwargs)
+        self.assert_valid_env(envs[0], Discrete)
         self.target_model = tf.keras.models.clone_model(self.model)
         self.target_model.set_weights(self.model.get_weights())
         self.double = double

xagents/tests/__init__.py
@@ -1,7 +1,1 @@
-import os
-import subprocess
-import sys
 
-if sys.platform == 'darwin':
-    subprocess.run(['xhost', '+'])
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'

xagents/tests/conftest.py
@@ -13,6 +13,8 @@ from xagents.utils.buffers import ReplayBuffer1, ReplayBuffer2
 from xagents.utils.cli import (agent_args, non_agent_args, play_args,
                                train_args, tune_args)
 
+optuna.logging.set_verbosity(optuna.logging.ERROR)
+
 
 @pytest.fixture(scope='function')
 def executor(request):

xagents/tests/test_base.py
@@ -104,7 +104,8 @@ class TestBase:
         Returns:
             agent_kwargs.
         """
-        agent_kwargs = {'envs': envs if envs is not None else self.envs}
+        valid_envs = self.envs if agent not in [TD3, DDPG] else self.envs2
+        agent_kwargs = {'envs': envs if envs is not None else valid_envs}
         buffers = buffers or self.buffers
         if self.model_counts[agent] > 1:
             agent_kwargs['actor_model'] = model or tf.keras.models.clone_model(
@@ -159,30 +160,31 @@ class TestBase:
             results.add(random.randint(0, test_range))
             results.add(int(np.random.randint(0, test_range, 1)))
             results.add(int(tf.random.uniform([1], maxval=test_range)))
-            results.add(self.envs[0].action_space.sample())
+            sample = agent.envs[0].action_space.sample()
+            if isinstance(sample, np.ndarray):
+                sample = tuple(sample)
+            results.add(sample)
         assert results1 == results2
 
     @pytest.mark.parametrize(
-        'env_id',
+        'env_ids',
         [
-            'RiverraidNoFrameskip-v4',
-            'Robotank-ramDeterministic-v0',
-            'BeamRiderDeterministic-v4',
-            'PongNoFrameskip-v4',
-            'GravitarNoFrameskip-v0',
-            'Freeway-ramDeterministic-v0',
-            'Bowling-ram-v0',
-            'CentipedeDeterministic-v0',
-            'Gopher-v4',
+            ('RiverraidNoFrameskip-v4', 'MountainCarContinuous-v0'),
+            ('Robotank-ramDeterministic-v0', 'Pendulum-v0'),
+            ('BeamRiderDeterministic-v4', 'LunarLanderContinuous-v2'),
+            ('PongNoFrameskip-v4', 'BipedalWalker-v3'),
+            ('GravitarNoFrameskip-v0', 'BipedalWalkerHardcore-v3'),
+            ('Freeway-ramDeterministic-v0', 'CarRacing-v0'),
         ],
     )
-    def test_n_actions(self, env_id, agent):
+    def test_n_actions(self, env_ids, agent):
         """
         Test if agent initializes the correct n_actions attribute.
         Args:
             env_id: One of agent ids available in xagents.agents
             agent: OnPolicy/OffPolicy subclass.
         """
+        env_id = env_ids[0] if agent not in [TD3, DDPG] else env_ids[1]
         env = gym.make(env_id)
         agent = agent(
             **self.get_agent_kwargs(agent, [env for _ in range(len(self.envs))])
@@ -190,7 +192,7 @@ class TestBase:
         if isinstance(env.action_space, Discrete):
             assert agent.n_actions == env.action_space.n
         else:
-            assert agent.n_actions == env.action_space.shape
+            assert agent.n_actions == env.action_space.shape[0]
 
     def test_unsupported_env(self, agent):
         """
@@ -593,14 +595,15 @@ class TestBase:
         """
         for buffer in self.buffers:
             buffer.main_buffer.clear()
+            buffer.current_size = 0
         agent = agent(**self.get_agent_kwargs(agent))
-        actions = np.random.randint(0, agent.n_actions, len(self.envs))
+        actions = [env.action_space.sample() for env in agent.envs]
         observations = agent.step_envs(actions, get_observation, store_in_buffers)
         if hasattr(agent, 'buffers'):
             if store_in_buffers:
-                assert all([len(buffer.main_buffer) > 0 for buffer in agent.buffers])
+                assert all([buffer.current_size > 0 for buffer in agent.buffers])
             else:
-                assert all([len(buffer.main_buffer) == 0 for buffer in agent.buffers])
+                assert all([buffer.current_size == 0 for buffer in agent.buffers])
         if get_observation:
             assert observations
             assert len(set([item.shape for item in observations[0]])) == 1